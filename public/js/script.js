661c9144d502c246f43827b9d013c9c6cb45982e
Abhishek0308 <agoyal0308@gmail.com>
2023-05-16 19:24:43 +0530

    update all files

--- public/js/script.js
+++ public/js/script.js
@@ -1,211 +1,93 @@
 
-(function ($) {
-	'use strict';
+// (function ($) {
+// 	'use strict';
 
-	// navbarDropdown
-	if ($(window).width() < 992) {
-		$('.navigation .dropdown-toggle').on('click', function () {
-			$(this).siblings('.dropdown-menu').animate({
-				height: 'toggle'
-			}, 300);
-		});
-  }
+// 	navbarDropdown
+// 	if ($(window).width() < 992) {
+// 		$('.navigation .dropdown-toggle').on('click', function () {
+// 			$(this).siblings('.dropdown-menu').animate({
+// 				height: 'toggle'
+// 			}, 300);
+// 		});
+//   }
 
-	// scroll to top button
-	$(window).on('scroll', function () {
-		if ($(window).scrollTop() > 70) {
-			$('.backtop').addClass('reveal');
-		} else {
-			$('.backtop').removeClass('reveal');
-		}
-	});
-	// scroll-to-top
-  $('.scroll-top-to').on('click', function () {
-    $('body,html').animate({
-      scrollTop: 0
-    }, 500);
-    return false;
-  });
+// 	// scroll to top button
+// 	$(window).on('scroll', function () {
+// 		if ($(window).scrollTop() > 70) {
+// 			$('.backtop').addClass('reveal');
+// 		} else {
+// 			$('.backtop').removeClass('reveal');
+// 		}
+// 	});
+// 	// scroll-to-top
+//   $('.scroll-top-to').on('click', function () {
+//     $('body,html').animate({
+//       scrollTop: 0
+//     }, 500);
+//     return false;
+//   });
 
-	$('.portfolio-single-slider').slick({
-		infinite: true,
-		arrows: false,
-		autoplay: true,
-		autoplaySpeed: 2000
-	});
-
-	$('.clients-logo').slick({
-		infinite: true,
-		arrows: false,
-		autoplay: true,
-		slidesToShow: 6,
-		slidesToScroll: 6,
-		autoplaySpeed: 6000,
-		responsive: [{
-				breakpoint: 1024,
-				settings: {
-					slidesToShow: 6,
-					slidesToScroll: 6,
-					infinite: true,
-					dots: true
-				}
-			},
-			{
-				breakpoint: 900,
-				settings: {
-					slidesToShow: 4,
-					slidesToScroll: 4
-				}
-			}, {
-				breakpoint: 600,
-				settings: {
-					slidesToShow: 4,
-					slidesToScroll: 4
-				}
-			},
-			{
-				breakpoint: 480,
-				settings: {
-					slidesToShow: 2,
-					slidesToScroll: 2
-				}
-			}
-
-		]
-	});
-
-	$('.testimonial-wrap').slick({
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		infinite: true,
-		dots: true,
-		arrows: false,
-		autoplay: true,
-		vertical: true,
-		verticalSwiping: true,
-		autoplaySpeed: 6000,
-		responsive: [{
-				breakpoint: 1024,
-				settings: {
-					slidesToShow: 1,
-					slidesToScroll: 1,
-					infinite: true,
-					dots: true
-				}
-			},
-			{
-				breakpoint: 900,
-				settings: {
-					slidesToShow: 1,
-					slidesToScroll: 1
-				}
-			}, {
-				breakpoint: 600,
-				settings: {
-					slidesToShow: 1,
-					slidesToScroll: 1
-				}
-			},
-			{
-				breakpoint: 480,
-				settings: {
-					slidesToShow: 1,
-					slidesToScroll: 1
-				}
-			}
-
-		]
-	});
-
-	$('.testimonial-wrap-2').slick({
-		slidesToShow: 2,
-		slidesToScroll: 2,
-		infinite: true,
-		dots: true,
-		arrows: false,
-		autoplay: true,
-		autoplaySpeed: 6000,
-		responsive: [{
-				breakpoint: 1024,
-				settings: {
-					slidesToShow: 2,
-					slidesToScroll: 2,
-					infinite: true,
-					dots: true
-				}
-			},
-			{
-				breakpoint: 900,
-				settings: {
-					slidesToShow: 1,
-					slidesToScroll: 1
-				}
-			}, {
-				breakpoint: 600,
-				settings: {
-					slidesToShow: 1,
-					slidesToScroll: 1
-				}
-			},
-			{
-				breakpoint: 480,
-				settings: {
-					slidesToShow: 1,
-					slidesToScroll: 1
-				}
-			}
-
-		]
-	});
+	
+	
 
 
-	// counter
-	function counter() {
-		var oTop;
-		if ($('.counter').length !== 0) {
-			oTop = $('.counter').offset().top - window.innerHeight;
-		}
-		if ($(window).scrollTop() > oTop) {
-			$('.counter').each(function () {
-				var $this = $(this),
-					countTo = $this.attr('data-count');
-				$({
-					countNum: $this.text()
-				}).animate({
-					countNum: countTo
-				}, {
-					duration: 500,
-					easing: 'swing',
-					step: function () {
-						$this.text(Math.floor(this.countNum));
-					},
-					complete: function () {
-						$this.text(this.countNum);
-					}
-				});
-			});
-		}
-  }
-  $(window).on('scroll', function () {
-		counter();
-	});
+// })(jQuery);
 
+// counter 
 
-	// Shuffle js filter and masonry
-	if ($('.shuffle-wrapper').length !== 0) {
-		var Shuffle = window.Shuffle;
-		var jQuery = window.jQuery;
-
-		var myShuffle = new Shuffle(document.querySelector('.shuffle-wrapper'), {
-			itemSelector: '.shuffle-item',
-			buffer: 1
-		});
-		jQuery('input[name="shuffle-filter"]').on('change', function (evt) {
-			var input = evt.currentTarget;
-			if (input.checked) {
-				myShuffle.filter(input.value);
-			}
-		});
+function counter() {
+	var oTop;
+	var counterElements = document.querySelectorAll('.counter');
+	if (counterElements.length !== 0) {
+	  oTop = counterElements[0].getBoundingClientRect().top - window.innerHeight;
 	}
+	if (window.pageYOffset > oTop) {
+	  counterElements.forEach(function (counterElement) {
+		var countTo = counterElement.getAttribute('data-count');
+		var countNum = parseFloat(counterElement.textContent);
+		var duration = 500;
+		var easing = 'swing';
+		var start = null;
+		var step = function (timestamp) {
+		  if (!start) {
+			start = timestamp;
+		  }
+		  var progress = timestamp - start;
+		  var percentage = Math.min(progress / duration, 1);
+		  var currentValue = countNum + percentage * (countTo - countNum);
+		  counterElement.textContent = Math.floor(currentValue);
+		  if (progress < duration) {
+			window.requestAnimationFrame(step);
+		  } else {
+			counterElement.textContent = countTo;
+		  }
+		};
+		window.requestAnimationFrame(step);
+	  });
+	}
+  }
+  
+  window.addEventListener('scroll', function () {
+	counter();
+  });
+  
 
-})(jQuery);
+  // shuffle doctors 
+
+if (document.querySelectorAll('.shuffle-wrapper').length !== 0) {
+	var Shuffle = window.Shuffle;
+	var jQuery = window.jQuery;
+	var myShuffle = new Shuffle(document.querySelector('.shuffle-wrapper'), {
+	  itemSelector: '.shuffle-item',
+	  buffer: 1
+	});
+	document.querySelectorAll('input[name="shuffle-filter"]').forEach(function(input) {
+	  input.addEventListener('change', function(evt) {
+		var input = evt.currentTarget;
+		if (input.checked) {
+		  myShuffle.filter(input.value);
+		}
+	  });
+	});
+  }
+  
